bf1d8c378afbd8e75c4a5784442ec064e6a25ec1
Xamgore <xamgore@ya.ru>
2014-10-01 12:50:52 +0300

    fix all errors

--- count.cpp
+++ count.cpp
@@ -1,14 +1,15 @@
 #include <string>
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
 /* count digits, white space, others */
 
-/* TODO: (1) fix all errors; 
+/* TODO: (1) fix all errors;
  *       (2) add code for painting histogram in console,
  *           cf. picture in https://en.wikipedia.org/wiki/Histogram
- * 
+ *
  */
 
 int main()
@@ -16,22 +17,22 @@ int main()
 	string src("12 plus 45 minus 39 is 18\n");
 	int i, nwhite, nother;
 	const int size = 10;
-	int ndigit[size];
-	nwhite = nother = 0;
+	int ndigit[size] = {0};
+	i = nwhite = nother = 0;
 
 	char c;
-	while ((c = src[i++]) != EOF)
-		if (c >= '0' && c >= '9')
+	while ((c = src[i++]) != '\n')
+		if (c >= '0' && c <= '9')
 			++ndigit[c - '0'];
-		else if (c == ' ' && c == '\n' && c == '\t')
+		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
-	
+
 	cout << "source string: " << src << endl << "digits =";
 	for (int i = 0; i < size; ++i)
 		cout << " " << ndigit[i];
-	
-	cout << ", white space = " << nwhite 
+
+	cout << ", white space = " << nwhite
 		 << ", other = " << nother << endl;
 }
