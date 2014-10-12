756aab75bc6adb5f99a1387fa641757967aee390
Jana Valieva <jana_valieva@list.ru>
2014-10-12 13:14:06 +0400

    Исправлены ошибки в count.cpp

--- count.cpp
+++ count.cpp
@@ -17,22 +17,25 @@ int main()
 	string src("12 plus 45 minus 39 is 18\n");
 	int i, nwhite, nother;
 	const int size = 10;
-	int ndigit[size];
+	int ndigit[size] = { 0 };
 	nwhite = nother = 0;
+	i = 0;
 
 	char c;
-	while ((c = src[i++]) != EOF)
-		if (c >= '0' && c >= '9')
+	while ((c = src[i++]) != '\n')
+		if (c >= '0' && c <= '9')
 			++ndigit[c - '0'];
-		else if (c == ' ' && c == '\n' && c == '\t')
+		else if ((c == ' ') || (c == '\n') || (c == '\t'))
 			++nwhite;
 		else
 			++nother;
 	
-	cout << "source string: " << src << endl << "digits =";
+	cout << "source string: " << src << endl << "digits = ";
 	for (int i = 0; i < size; ++i)
-		cout << " " << ndigit[i];
+		cout << ndigit[i] << " ";
 	
 	cout << ", white space = " << nwhite 
 		 << ", other = " << nother << endl;
+
+	system("pause");
 }
