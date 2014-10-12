638917a05cce03c99518c759b93a52f192d5da12
Jana Valieva <jana_valieva@list.ru>
2014-10-12 12:40:35 +0400

    task_7(): исправлена функция печати

--- arrays.cpp
+++ arrays.cpp
@@ -55,15 +55,15 @@ void task_6_poor_copy(const int * arr1, int * arr2, int size)
 	for (int i = 0; i < size; i++)
 		arr2[i] = arr1[i];
 }
-#if 0
+
 // Task 7
 // print array `arr`
-void task_7_print(int * arr, int size, char delim)
+void task_7_print(const int * arr, int size, char delim = ' ')
 {
-	// Hint: something wrong here; run this from main()
-	for (int i = 0; i < size; ++i)
+	for (int i = 0; i < size; i++)
 	{
 		std::cout << arr[i] << delim;
 	}
 
-#endif
+
+}
