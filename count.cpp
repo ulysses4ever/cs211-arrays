f55d0b095237574c8b9dcc1659edcf6ab4199dd7
artking4815162342 <artur_96@bk.ru>
2015-10-06 23:40:23 +0300

    исправлен основной блок кода

--- count.cpp
+++ count.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
@@ -14,19 +15,25 @@ using namespace std;
 int main()
 {
 	string src("12 plus 45 minus 39 is 18\n");
-	int i, nwhite, nother;
+	int nwhite, nother;
 	const int size = 10;
-	int ndigit[size];
+	int ndigit[size]{0};
 	nwhite = nother = 0;
 
 	char c;
-	while ((c = src[i++]) != EOF)
-		if (c >= '0' && c >= '9')
+
+	for (int j = 0; j < src.length(); j ++)
+	{
+		c = src[j];
+
+		if (c >= '0' && c <= '9')
 			++ndigit[c - '0'];
-		else if (c == ' ' && c == '\n' && c == '\t')
+		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
+	}
+	
 	
 	cout << "source string: " << src << endl << "digits =";
 	for (int i = 0; i < size; ++i)
@@ -34,4 +41,6 @@ int main()
 	
 	cout << ", white space = " << nwhite 
 		 << ", other = " << nother << endl;
+
+	system("pause");
 }
