c48fd6e269de29917de298ddf159a9e4d84a5510
artking4815162342 <artur_96@bk.ru>
2015-10-06 23:03:37 +0300

    исправлены все функции файла array.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cassert>
 
-using namespace std;
-
 void task_1()
 {
     /* Task 1: find a bug in the following declaration.
@@ -30,7 +28,7 @@ int task_3_zero()
     // Task 3
     // Hint 1: fix with initialization (not assignment)
     // Hint 2: you can use only one zero and not five of them! cf. task 2
-    int a[5];
+	int a[5] = { 0 };
     
     assert(!a[4]);
     return a[4]; // don't touch this!
@@ -40,7 +38,7 @@ void task_4()
 {
     // Task 4: asserts at the end must hold
     // Hint: fix with initialization (not assignment)
-    int a[5];
+	int a[5]{1, 2, 3, 4, 5};
     
     assert(a[0] == 1);
     assert(a[1] == 2);
@@ -53,32 +51,29 @@ void task_4()
 // copy arr1 to arr2
 void task_5_copy(int * arr1, int * arr2, int size)
 {
-    arr2 = arr1;
+	for (int i = 0; i < size; i++)
+	{
+		arr2[i] = arr1[i];
+	}
 }
 
 // Task 6
 // copy array `arr1` to array `arr2` of the same size
-void task_6_poor_copy(int * arr1, int * arr2)
+void task_6_poor_copy(int * arr1, int * arr2, int size)
 {
-    // Hint: something wrong here; test this from main()
-    for(int i = 0; i < sizeof(arr2); ++i)
+    for(int i = 0; i < size; ++i)
     {
         arr2[i] = arr1[i];
     }
 }
 
-/*
 // Task 7
 // print array `arr`
 void task_7_print(int * arr, int size, char delim = ' ')
 {
-    // Hint: something wrong here; run this from main()
-    for(int i = 0; i <= size; ++i)
+    for(int i = 0; i < size; ++i)
     {
-        std::cout << a[i] << delim;
+        std::cout << arr[i] << delim;
     }
+	std::cout << std::endl;
 }
-*/
-
-
-
--- arrays.h
+++ arrays.h
@@ -5,4 +5,17 @@ void task_1();
 
 void task_2();
 
+int task_3_zero();
+
+void task_4();
+
+// copy arr1 to arr2
+void task_5_copy(int * arr1, int * arr2, int size);
+
+// copy array `arr1` to array `arr2` of the same size
+void task_6_poor_copy(int * arr1, int * arr2, int size);
+
+// print array `arr`
+void task_7_print(int * arr, int size, char delim = ' ');
+
 #endif
