db1d2b36a38c68cadc3dd5c836ac346e8608bcb0
sarieved <sarieved@gmail.com>
2015-10-09 01:36:55 +0300

    В заголовочный файл добавлены заголовки всех ф-ий

--- arrays.h
+++ arrays.h
@@ -1,7 +1,26 @@
 #ifndef assert_h
 #define assert_h
 
+// Task 1
+void task_1();
 
+// Task 2
+void task_2();
+
+// returns zero
+int task_3_zero();
+
+// Task 4
+void task_4();
+
+// Task 5
+void task_5_copy(int * arr1, int * arr2, int size);
+
+// Task 6
+void task_6_poor_copy(int * arr1, int * arr2, int size);
+
+// Task 7
+void task_7_print(const int * arr, int size, char delim = ' ');
 
 //assert_h
 #endif
