d4a91cb95d3cc82c19eb43d74c0e98f0d040c0a0
sarieved <sarieved@gmail.com>
2015-10-09 01:36:07 +0300

    Исправлена ф-ия task_7_print
    1)Параметр ф-ии int * arr изменён на const int * arr, т.к. в ходе печати
    массив не должен изменяться
    2)Значение по умолчанию переменной delim должно быть отображено только в
    заголовочном файле
    3)Цикл выолняется пока i строго меньше size, иначе неизбежен выход за границы
    массива
    4)В печати переменная a заменена на переменную arr, т.к. a - неопределена

--- arrays.cpp
+++ arrays.cpp
@@ -56,16 +56,12 @@ void task_6_poor_copy(int * arr1, int * arr2, int size)
     }
 }
 
-#if 0
+
 // Task 7
-// print array `arr`
-void task_7_print(int * arr, int size, char delim = ' ')
+void task_7_print(const int * arr, int size, char delim)
 {
-    // Hint: something wrong here; run this from main()
-    for(int i = 0; i <= size; ++i)
+    for(int i = 0; i < size; ++i)
     {
-        std::cout << a[i] << delim;
+        std::cout << arr[i] << delim;
     }
 }
-
-#endif
